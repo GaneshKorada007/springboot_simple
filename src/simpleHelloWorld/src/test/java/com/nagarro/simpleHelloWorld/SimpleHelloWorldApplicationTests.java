49aaf0e8607ff5677b36fa3ac611ec0acd0ff030
ganeshkorada <ganesh.korada@nagarro.com>
2024-04-03 19:27:28 +0530

    commited

---
+++ src/simpleHelloWorld/src/test/java/com/nagarro/simpleHelloWorld/SimpleHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.simpleHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
