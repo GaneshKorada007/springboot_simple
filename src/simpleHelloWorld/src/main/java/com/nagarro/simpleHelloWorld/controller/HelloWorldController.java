1ce134d024ad4fa53910552e95d1734b6699643d
KORADA GANESH <139643802+GaneshKorada007@users.noreply.github.com>
2024-04-03 21:28:17 +0530

    Update HelloWorldController.java

--- src/simpleHelloWorld/src/main/java/com/nagarro/simpleHelloWorld/controller/HelloWorldController.java
+++ src/simpleHelloWorld/src/main/java/com/nagarro/simpleHelloWorld/controller/HelloWorldController.java
@@ -8,10 +8,8 @@ public class HelloWorldController {
 
 	 @GetMapping("/")
 	    public String hello() {
-<<<<<<< HEAD
+
 	        return "Hello, world! Thank you";
-=======
-	        return "Hello, world! thank you";
->>>>>>> 42fec32bbbce56e472a9a690f4ef007b34a2b266
+
 	    }
 }
